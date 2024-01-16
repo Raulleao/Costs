af121b92fdbada04fd772df5314c161681310187
Raulleao <raulleaochagas@gmail.com>
2024-01-16 13:07:04 -0400

    feat: import component input

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,10 +1,23 @@
+import styles from "./ProjectForm.module.css";
+import Input from "../form/Input";
+
 function ProjectForm() {
   var valueSubmit = "Create Project";
   return (
-    <form>
-      <div>
-        <input type="text" placeholder="Enter the name project" />
-      </div>
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Project name:"
+        name="name"
+        placeholder="Enter the project name"
+      />
+      <Input
+        type="number"
+        text="Project budget:"
+        name="budget"
+        placeholder="Enter the budget"
+      />
+
       <div>
         <input type="number" placeholder="Enter the budget" />
       </div>
